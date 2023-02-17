d62e7a79116b4c4a63ca450f988b4f63470dc7ee
Berkay Berkman <berkayberkman96@gmail.com>
2023-02-17 03:17:12 +0300

    add timeout to the milvus requests

--- api/src/milvus_handler.py
+++ api/src/milvus_handler.py
@@ -22,8 +22,8 @@ class MilvusHandler:
         search_limit = MILVUS_DB_SEARCH_LIMIT
         search_expr = None
 
-        similar_embeddings = self.collection.search(embeddings, search_field, param=search_params, limit=search_limit, expr=search_expr)
+        similar_embeddings = self.collection.search(embeddings, search_field, param=search_params, limit=search_limit, expr=search_expr, timeout=MILVUS_DB_CONNECTION_TIMEOUT)
         return similar_embeddings
 
     def insert_entry(self, entry_id: int, entry_embeddings: np.ndarray):
-        return self.collection.insert([[entry_id], entry_embeddings])
+        return self.collection.insert([[entry_id], entry_embeddings], timeout=MILVUS_DB_CONNECTION_TIMEOUT)
